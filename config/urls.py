21aefd88cb7b31070b1f981e3b73c88485eb67f7
felipedi2000 <felip.edi2000@gmail.com>
2026-09-13 20:29:18 -0500

    Fix drf-spectacular import name

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from config import settings
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularReDocView, SpectacularAPIView
-  
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+    SpectacularAPIView,
+) 
 urlpatterns = [
   path("api/v1/auth/", include("apps.authentication.urls")),
   path("api/v1/wallet/", include(("apps.wallet.urls", "wallet"), namespace="wallet")),
   path("api/v1/transactions/", include(("apps.transactions.urls", "transactions"), namespace="transactions")),
   path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
   path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-  path("api/redoc/", SpectacularReDocView.as_view(url_name="schema"), name="redoc"),
+  path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
 if settings.ENABLE_ADMIN:
